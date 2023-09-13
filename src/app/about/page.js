817c927769104075062f2dfc84228ada28071dc7
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-13 12:26:07 +0100

    Trabajando en el componente About

---
+++ src/app/about/page.js
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div>
+      <h1 className={styles.about}>About</h1>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.container}>
       <h1 className={styles.titulo}>Aprendiendo Next.js</h1>
+      <Link className={styles.about} href="/about">
+        Sobre mi
+      </Link>
     </main>
   );
 }
