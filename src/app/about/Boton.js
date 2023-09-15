2ce46b1273fbc65caccc90b8ad38c6f8385f0cf7
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-15 16:19:59 +0100

    Trabajando con el componente Boton.js

--- src/app/about/Boton.js
+++ src/app/about/Boton.js
@@ -1,7 +1,12 @@
 "use client";
+import styles from "./about.module.css";
 import React, { useState } from "react";
 
 export default function Boton() {
   const [color, setColor] = useState("red");
-  return <button onClick={() => setColor("yelow")}>{color}</button>;
+  return (
+    <button className={styles.btnAbout} onClick={() => setColor("yelow")}>
+      {color}
+    </button>
+  );
 }
