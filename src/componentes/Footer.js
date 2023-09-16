871577d4a9f0de056bd6c9316fa946868607237e
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-16 12:49:40 +0100

    Cambiando el footer a un componente

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,12 +1,9 @@
 import styles from "./footer.module.css";
 
-export const dynamic = "force-dynamic";
-
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Página renderizada en: </p>
-      {new Date().toLocaleString("es-ES")}
+      <p>Página renderizada en: {new Date().toLocaleString("es-ES")}</p>
     </footer>
   );
 }
