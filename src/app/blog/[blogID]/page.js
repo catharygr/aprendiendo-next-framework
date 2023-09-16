9e74cca3bf23a14b966f8f0c5591b791df5a2172
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-16 15:20:26 +0100

    Trabajando con la lógica del contador

--- src/app/blog/[blogID]/page.js
+++ src/app/blog/[blogID]/page.js
@@ -2,40 +2,27 @@
 "use client";
 import { data } from "@/app/data";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export default function BlogPagina({ params }) {
-  const [contador, setContador] = useState(0);
   const blogID = params.blogID;
   const router = useRouter();
 
   const usuario = data.find((usuario) => usuario.id === blogID);
 
-  // Poner un contador de tiempo antes que regrese a la página principal y prsentarlo en la página
-
   useEffect(() => {
-    const tiempo = setInterval(() => {
-      setContador((prevContador) => prevContador - 1);
-    }, 5000);
-
-    if (contador === 0) {
-      clearInterval(tiempo);
+    const tiempo = setTimeout(() => {
       router.push("/");
-    }
+    }, 5000);
     return () => {
-      clearInterval(tiempo);
+      clearTimeout(tiempo);
     };
   }, []);
 
   return (
     <div>
       <p>{usuario.name}</p>
-      <p>
-        Regresaremos en:{" "}
-        {contador === 0
-          ? "al instante"
-          : `${contador} segundo${contador !== 1 ? "s" : ""}`}
-      </p>
+      <p>Regresaremos en dos segundo</p>
     </div>
   );
 }
