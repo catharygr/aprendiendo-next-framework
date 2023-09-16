1263bf9721909754bbe6433a399c8fbd248b3608
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-16 11:04:33 +0100

    Terminada con la dinámica del componente Layout

--- src/app/blog/[blogID]/page.js
+++ src/app/blog/[blogID]/page.js
@@ -1,5 +1,14 @@
 import { data } from "@/app/data";
 
+export async function generateMetadata({ params }) {
+  const blogID = params.blogID;
+
+  const usuario = data.find((usuario) => usuario.id === blogID);
+  return {
+    title: usuario.name,
+  };
+}
+
 export default function BlogPagina({ params }) {
   const blogID = params.blogID;
 
--- src/layoutNosotros.js/page.js
+++
@@ -1,9 +0,0 @@
-import { data } from "@/app/data";
-
-export async function generateMetadata({ params }) {
-  const userID = params.userID;
-  const usuario = data.find((usuario) => usuario.id === userID);
-  return {
-    title: `${usuario.name}`,
-  };
-}
