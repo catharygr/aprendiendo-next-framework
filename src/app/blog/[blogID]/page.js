a10faf33f0afff3fbfd674a4256b89d11b0b107f
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-16 15:11:51 +0100

    Trabajar en un contador en setInterval

--- src/app/blog/[blogID]/page.js
+++ src/app/blog/[blogID]/page.js
@@ -2,29 +2,40 @@
 "use client";
 import { data } from "@/app/data";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function BlogPagina({ params }) {
+  const [contador, setContador] = useState(0);
   const blogID = params.blogID;
   const router = useRouter();
 
   const usuario = data.find((usuario) => usuario.id === blogID);
 
-  // Poner un contador de tiempo para que regrese a la página principal
+  // Poner un contador de tiempo antes que regrese a la página principal y prsentarlo en la página
 
   useEffect(() => {
-    const tiempo = setTimeout(() => {
+    const tiempo = setInterval(() => {
+      setContador((prevContador) => prevContador - 1);
+    }, 5000);
+
+    if (contador === 0) {
+      clearInterval(tiempo);
       router.push("/");
-    }, 2000);
+    }
     return () => {
-      clearTimeout(tiempo);
+      clearInterval(tiempo);
     };
   }, []);
 
   return (
     <div>
       <p>{usuario.name}</p>
-      <p>Regresaremos en dos segundo</p>
+      <p>
+        Regresaremos en:{" "}
+        {contador === 0
+          ? "al instante"
+          : `${contador} segundo${contador !== 1 ? "s" : ""}`}
+      </p>
     </div>
   );
 }
