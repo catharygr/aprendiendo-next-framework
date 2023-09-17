fb0785d7a4855a87c03c3acf6f988dd969711a9c
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-17 15:59:48 +0100

    Practicando carga diferida en Next

--- src/app/latex/page.js
+++ src/app/latex/page.js
@@ -1,7 +1,27 @@
-export default function LatexPagina() {
+"use client";
+
+import dynamic from "next/dynamic";
+import React, { useState } from "react";
+
+const Latex = dynamic(() => import("react-latex-next"));
+
+function LatexEditor() {
+  const [expression, setExpression] = React.useState("$2^4 - 4$");
+
   return (
     <div>
-      <p>Latex Pagina</p>
+      <label htmlFor="expression-input">Enter LaTeX</label>
+      <input
+        id="expression-input"
+        type="text"
+        value={expression}
+        onChange={(event) => setExpression(event.target.value)}
+      />
+
+      <h2>Output:</h2>
+      <Latex>{expression}</Latex>
     </div>
   );
 }
+
+export default LatexEditor;
