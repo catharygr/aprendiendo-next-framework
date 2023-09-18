8bd30f178f1d36fc5ad896416f37975eb64cf7af
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-18 12:33:16 +0100

    Practicando libreria Framer motion

--- src/app/framer/page.js
+++ src/app/framer/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import styles from "./framer.module.css";
+import { motion } from "framer-motion";
 
 export default function FramerPage() {
   const [esFramer, setEsFramer] = React.useState(false);
@@ -8,7 +9,7 @@ export default function FramerPage() {
   return (
     <div className={styles.container}>
       <h1>Framer Motion</h1>
-      <div className={styles.pelota} />
+      <motion.div className={styles.pelota} animate={{ scale: 0.5 }} />
       <button className={styles.btn} onClick={() => setEsFramer(!esFramer)}>
         Toggle
       </button>
