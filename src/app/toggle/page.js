c6d64ec612783c95957d05ba288d290b1c332bd2
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-18 13:48:34 +0100

    Practicando libreria Framer motion

--- src/app/toggle/page.js
+++ src/app/toggle/page.js
@@ -4,10 +4,13 @@ import styles from "./toggle.module.css";
 import { motion } from "framer-motion";
 
 export default function TogglePage() {
+  const [esOn, setEsOn] = useState(false);
   return (
     <div className={styles.container}>
-      <h1 className={styles.h1}>Framer Motion</h1>
-      Toggle
+      <h1 className={styles.h1}>Toggle</h1>
+      <button className={styles.btn}>
+        <motion.span className={styles.pelota} />
+      </button>
     </div>
   );
 }
